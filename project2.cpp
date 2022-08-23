33415fa29a70d7db9d2510a8bd4fe3409909c9c9
GeekyHichambel <109455495+GeekyHichambel@users.noreply.github.com>
2022-08-23 19:46:16 +0530

    Update project2.cpp

--- project2.cpp
+++ project2.cpp
@@ -7,15 +7,20 @@ int main()
 int l,b,h ;
 cout << "Enter the lenght :";/*lenght*/
 cin >> l;
+
 cout << "Enter the breadth :";/*breadth*/
 cin >> b;
+
 cout << "Enter the height :";/*height*/
 cin >> h;
+
 int v,di;
 v=l*b*h;
-di=sqrt(l*l+b*b+h*h);
+di=sqrt(l*l+b*b+h*h);/*diagonal of a cuboid*/
+
 cout << "The volume is :" << v;
 cout << endl;
 cout << "The diagonal is :" << di;
+
 return(0);
 }
