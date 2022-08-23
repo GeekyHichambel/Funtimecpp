59768b68574aff55b68b9c69e72412c425ad2305
GeekyHichambel <109455495+GeekyHichambel@users.noreply.github.com>
2022-08-23 19:35:35 +0530

    Add files via upload

---
+++ project2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+/*to find the volume of a cuboid*/
+int main()
+{
+int l,b,h ;
+cout << "Enter the lenght :";/*lenght*/
+cin >> l;
+cout << "Enter the breadth :";/*breadth*/
+cin >> b;
+cout << "Enter the height :";/*height*/
+cin >> h;
+int v,di;
+v=l*b*h;
+di=sqrt(l*l+b*b+h*h);
+cout << "The volume is :" << v;
+cout << endl;
+cout << "The diagonal is :" << di;
+return(0);
+}
