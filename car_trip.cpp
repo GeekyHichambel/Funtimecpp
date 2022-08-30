95be9b397a2f8c3ec851babe0b57a8a1a4a415be
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2022-08-30 17:58:31 +0530

    Update car_trip.cpp

--- car_trip.cpp
+++ car_trip.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 int main()
 {
- int x,total,t;
- cout << "Enter the number of kms travelled :";
- cin >> x;
- cout << "Enter the number of test cases :";
- cin >> t;
- for (t<=100;t++;) 
+ int x,total,t,n;
+ cout << "Enter the number of test cases : ";
+  cin>> n;
+  for (t=0;t<n;t++)
  {
+     cout << "Enter the number of kms travelled :";
+     cin >> x;
   if (x<=300)
   {
   cout << "The total cost of travel is 3000 rupees"<< endl;
@@ -19,6 +19,7 @@ int main()
   	total=x*10;
   	cout << total << " rupees" << endl; 
   }
- return (0);
+ cout << endl;
 }
+return (0);
 }
