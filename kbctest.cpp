488fe15703ae24ef17420e0f524ff08eeb375c13
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2022-09-02 23:09:01 +0530

    Add files via upload

--- kbctest.cpp
+++ kbctest.cpp
@@ -4,43 +4,46 @@
 using namespace std;
  int main()
  {
-    cout << "Welcome to KBC" <<endl;
+   cout << "Welcome to KBC" << endl;
   int a;
   string name,gender;
   
-  cout << "Enter your name : ";
+  cout << "Enter your name : " ;
   cin >> name;
   
 
-  cout << "Your age here : ";
+  cout << "Your age here : " ;
   cin >> a;
 
-  cout << "Your gender here : ";
+  cout<< "Your gender here : " ;
   cin >> gender;
-  cout <<endl;
+  cout << endl;
 
-  cout << "Your details are given below: " << endl;
+  cout<< "Your details are given below: " << endl;
   cout << " || ";
-  cout << "Name : " << name;
+  cout  << "Name : " << name ;
   cout << " || ";
-  cout << "Gender : "<< gender;
+  cout << "Gender : "<< gender ;
   cout << " || ";
-  cout << "Age : "<<a;
+  cout << "Age : "<<a ;
   cout << " || ";
 
-  cout << "Enter '1' for starting and '0' for exiting  ";int i;
+  cout << "Enter '1' for starting and '0' for exiting  ";
+  int i;
   cin >> i;
   if (i==1)
   {
    cout << "Your first Question is : " <<endl;
+   
    cout << "What is the national game of India?"<<endl;
+   
    cout << "Option A : CRICKET   || Option B : HOCKEY"<<endl;
+   
    cout << "Option C : BADMINTON || Option D : TENNIS"<<endl;
-   char A[1],B[1],C[1],D[1];
-   char O[1];
-   cout << "Enter your Choice in terms of A,B,C,D"<<endl;
-   cin.getline(O, sizeof(O));
-     if (O==B)
+   int opt;
+   cout << " <<   <<  Enter your Choice in terms of 1,2,3,4  >>   >> "<<endl;
+   cin >> opt;
+     if (opt==2)
              {
                cout << "Your answer is correct." <<endl;
               }
