e51a2cc86d3ac0af87e131d6c7f363151d36fe7e
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2022-09-10 11:40:02 +0530

    Update operation.cpp

--- randoms/operation.cpp
+++ randoms/operation.cpp
@@ -8,23 +8,35 @@
 	cout << "Enter the second number : ";
 	cin >>b;
 	cout << endl;
-    cout << "Enter 1 for sum, 2 for subtraction,3 for division and 4 for product ";
+    cout << "Enter 1 for sum, 2 for subtraction,3 for division, 4 for product and 5 for all ";
     cin >> o;
+
     if (o==1)
     {
 	cout <<"The sum of both the numbers is :" << a+b <<endl;
 	}
-if (o==2)
-   {
-	cout <<"The difference of both the numbers is :" << a-b<<endl;
-   }
-if (o==3)
-   {
-    cout <<"The division of both the numbers is :" << a/b<<endl;
-   }
+
+    if (o==2)
+    {
+	 cout <<"The difference of both the numbers is :" << a-b <<endl;
+    }
+
+    if (o==3)
+    {
+     cout <<"The division of both the numbers is :" << a/b <<endl;
+    }
+
    if (o==4)
    {
-    cout <<"The product of both the numbers is :" << a*b<<endl;
+    cout <<"The product of both the numbers is :" << a*b <<endl;
  	}
+    if (o==5)
+    {
+        cout <<"The sum of both the numbers is :" << a+b <<endl;
+        cout <<"The difference of both the numbers is :" << a-b <<endl;
+        cout <<"The division of both the numbers is :" << a/b <<endl;
+        cout <<"The product of both the numbers is :" << a*b <<endl;
+    }
+ 	
  	return 0;
  }
